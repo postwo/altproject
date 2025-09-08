5feabcc3bf66bf81f35f0e16d3845d0664425e7c
postwo <codo7717@naver.com>
2025-09-09 08:40:33 +0900

    세팅

---
+++ src/main/java/com/example/altproject/AltprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.altproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltprojectApplication.class, args);
+	}
+
+}
