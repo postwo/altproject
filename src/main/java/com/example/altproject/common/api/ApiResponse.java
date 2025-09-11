d6a4aeef5fe4bdf7bcc80e311415af3a2bbd18ea
postwo <codo7717@naver.com>
2025-09-11 18:29:56 +0900

    ApiResponse suceess,validation 추가 globalexception도 추가

--- src/main/java/com/example/altproject/common/api/ApiResponse.java
+++ src/main/java/com/example/altproject/common/api/ApiResponse.java
@@ -9,12 +9,33 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ApiResponse {
+public class ApiResponse<T> {
     private String code;
     private String message;
     private int status;
+    private T data;
 
-    public static ApiResponse ApiError(ApiException apiException) {
+    public static <T> ApiResponse<T> Success(T data) {
+        var successStatus = ErrorStatus.SUCCESS;
+        return ApiResponse.<T>builder()
+                .code(successStatus.getCode())
+                .message(successStatus.getDescription())
+                .status(successStatus.getHttpStatus())
+                .data(data)
+                .build();
+    }
+
+    public static ApiResponse<?> ValidationException() {
+        var errorStatus = ErrorStatus.VALIDATION_FAIL;
+
+        return ApiResponse.builder()
+                .code(errorStatus.getCode())
+                .message(errorStatus.getDescription())
+                .status(errorStatus.getHttpStatus())
+                .build();
+    }
+
+    public static ApiResponse<?> ApiError(ApiException apiException) {
 
         var errorStatus = apiException.getErrorStatus();
 
@@ -30,7 +51,7 @@ public class ApiResponse {
     }
 
 
-    public static ApiResponse GlobalError() {
+    public static ApiResponse<?> GlobalError() {
 
         var errorStatus = ErrorStatus.SERVER_ERROR;
 
--- src/main/java/com/example/altproject/exceptionhandler/GlobalExceptionHandler.java
+++ src/main/java/com/example/altproject/exceptionhandler/GlobalExceptionHandler.java
@@ -5,8 +5,10 @@ import com.example.altproject.common.api.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 
 @Slf4j
@@ -15,10 +17,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(value = Exception.class)
-    public ResponseEntity<ApiResponse> execption(Exception exception){
+    public ResponseEntity<ApiResponse<?>> execption(Exception exception){
         log.error("GLOBAL EXCEPTION 발생 : ",exception);
 
         return ResponseEntity.status(ErrorStatus.SERVER_ERROR.getHttpStatus()).body(ApiResponse.GlobalError());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleValidationException(MethodArgumentNotValidException ex) {
+        log.error("VALIDATION EXCEPTION 발생 : ",ex);
+
+        return ResponseEntity.status(ErrorStatus.VALIDATION_FAIL.getHttpStatus()).body(ApiResponse.ValidationException());
+    }
 }
