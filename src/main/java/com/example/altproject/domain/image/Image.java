99dd9fdd708817fa890b86fba075e60be03293b9
postwo <codo7717@naver.com>
2025-09-14 20:24:40 +0900

    Image entity

---
+++ src/main/java/com/example/altproject/domain/image/Image.java
@@ -0,0 +1,42 @@
+package com.example.altproject.domain.image;
+
+import com.example.altproject.domain.board.Board;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "image")
+@ToString(exclude = "board")
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    private String image;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Image that = (Image) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
