ccc882c77e0b5ddc4ce517f02c871efeddaca343
postwo <codo7717@naver.com>
2025-09-18 21:18:23 +0900

    중복된 iocexception삭제

--- src/main/java/com/example/altproject/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/example/altproject/handler/OAuth2LoginSuccessHandler.java
@@ -35,7 +35,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
-                                        Authentication authentication) throws IOException, ServletException, IOException {
+                                        Authentication authentication) throws IOException, ServletException {
 
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
 
