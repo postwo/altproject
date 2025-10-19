235dbe0487ea53c597a35e2f264ba81bb9f7947d
postwo <codo7717@naver.com>
2025-10-19 22:54:12 +0900

    chat domain 작성

---
+++ src/main/java/com/example/altproject/chat/domain/ChatMessage.java
@@ -0,0 +1,37 @@
+package com.example.altproject.chat.domain;
+
+import com.example.altproject.common.auditing.AuditingFields;
+import com.example.altproject.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class ChatMessage  extends AuditingFields {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    @Column(nullable = false, length = 500)
+    private String content;
+
+    @OneToMany(mappedBy = "chatMessage", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<ReadStatus> readStatuses = new ArrayList<>();
+}
---
+++ src/main/java/com/example/altproject/chat/domain/ChatParticipant.java
@@ -0,0 +1,28 @@
+package com.example.altproject.chat.domain;
+
+import com.example.altproject.common.auditing.AuditingFields;
+import com.example.altproject.domain.member.Member;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class ChatParticipant extends AuditingFields {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "chat_room_id", nullable = false)
+    private ChatRoom chatRoom;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+}
